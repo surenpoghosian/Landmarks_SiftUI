80444f3583613cd06f7f44da6582df702077df0f
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2024-01-05 21:48:05 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Suren Poghosyan on 05.01.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
